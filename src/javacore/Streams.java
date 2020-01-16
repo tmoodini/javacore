0da3d618da9c07d52a230c27f4827b0d9ffe9dae
tmoodini <tom.moody@gmail.com>
2020-01-16 09:13:03 -0700

    Another commit

--- src/javacore/Streams.java
+++ src/javacore/Streams.java
@@ -5,6 +5,11 @@ public class Streams {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO");
+		String x = "Starter";
+		System.out.println(x);
+		x = "changed";
+		System.out.println(x);
+		
 	}
 
 }
