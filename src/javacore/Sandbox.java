de6737d0f4279578d8cddae92451647176df7023
tmoodini <tom.moody@gmail.com>
2020-01-16 11:23:52 -0700

    Switched some stuff around

--- src/javacore/Sandbox.java
+++ src/javacore/Sandbox.java
@@ -3,7 +3,10 @@ package javacore;
 public class Sandbox {
 
 	public static void main(String[] args) {
-		
+		String x = "Starter";
+		System.out.println(x);
+		x = "changed";
+		System.out.println(x);
 
 	}
 
--- src/javacore/Streams.java
+++ src/javacore/Streams.java
@@ -5,10 +5,7 @@ public class Streams {
 	public static void main(String[] args) {
 		
 		System.out.println("HELLO");
-		String x = "Starter";
-		System.out.println(x);
-		x = "changed";
-		System.out.println(x);
+		
 		
 	}
 
