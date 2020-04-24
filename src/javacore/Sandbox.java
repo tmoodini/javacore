8e4bdcb85c113325202c2137343e5d3a810692e7
tmoodini <tom.moody@gmail.com>
2020-04-24 16:09:50 -0600

    Cleanup

--- src/javacore/Sandbox.java
+++ src/javacore/Sandbox.java
@@ -1,12 +1,26 @@
 package javacore;
 
-public class Sandbox {
+import java.util.Random;
 
+public class Sandbox {
+	public static enum Result {FIRST, SECOND, THIRD}
 	public static void main(String[] args) {
-		String x = "Starter";
-		System.out.println(x);
-		x = "changed";
-		System.out.println(x);
+		Random random = new Random();
+		for( int i = 0; i < 10; i++) {
+			Result randomResult = 
+					 Result.values()[random.nextInt(Result.values().length)];
+			System.out.println(randomResult);
+		}
+		
+		boolean test = true;
+		test = !test;
+		
+		System.out.println(test);
+		
+		
+		
+		
+		
 
 	}
 
