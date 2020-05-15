215583a6d94bea11737ff677aec79f96ffbd38ab
tmoodini <tom.moody@gmail.com>
2020-05-14 20:09:58 -0600

    Updated the BBtree and added a treedatafile.txt for testing

--- src/com/tmoodini/datastructures/BalancedBinaryTree.java
+++ src/com/tmoodini/datastructures/BalancedBinaryTree.java
@@ -1,6 +1,10 @@
 //adding some notes
 
 package com.tmoodini.datastructures;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class BalancedBinaryTree {
 	TreeNode root;
@@ -27,11 +31,68 @@ public class BalancedBinaryTree {
 		
 	}
 	
+	public void insert(TreeNode treeNode, TreeNode currentNode) {
+		
+		
+		if(this.root == null) {
+			this.root = treeNode;
+			return;
+		}
+		
+		if(treeNode.key <= currentNode.key) {
+			if(currentNode.left == null) {
+				currentNode.left = treeNode;
+			}
+			else {
+				insert(treeNode, currentNode.left);
+			}
+			
+		}
+		
+		if(treeNode.key > currentNode.key) {
+			if(currentNode.right == null) {
+				currentNode.right = treeNode;
+			}
+			else {
+				insert(treeNode, currentNode.right);
+			}
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		BalancedBinaryTree btree = new BalancedBinaryTree(1);
-		btree.root.left = new TreeNode(2);
-		btree.root.right = new TreeNode(3);
+		BufferedReader reader;
+		//System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		BalancedBinaryTree btree = new BalancedBinaryTree();
+		try {
+			reader = new BufferedReader(new FileReader(
+					"./testdata/treedatafile.txt"));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+				line = reader.readLine();
+				
+				if(line != null) {
+				
+				TreeNode node = new TreeNode(Integer.parseInt(line));
+				btree.insert(node, btree.root);
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//btree.root.left = new TreeNode(2);
+		//btree.root.right = new TreeNode(3);
 		
 	}
 
