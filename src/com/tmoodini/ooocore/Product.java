7405e30e80fb826dc78fe6b4d89913eb8cc0abc6
Tom Moody <tom.moody@gmail.com>
2024-02-15 14:21:06 -0700

    Started ooo section in new package

---
+++ src/com/tmoodini/ooocore/Product.java
@@ -0,0 +1,10 @@
+package com.tmoodini.ooocore;
+
+import java.math.BigDecimal;
+
+public interface Product {
+
+    public int getProductID();
+    public BigDecimal getPrice();
+
+}
---
+++ src/com/tmoodini/ooocore/Vehicle.java
@@ -0,0 +1,9 @@
+package com.tmoodini.ooocore;
+
+public interface Vehicle {
+
+    public boolean motorized();
+    public void setMotorized(boolean motorized);
+    public void setMaxSpeed(double maxspeed);
+    public double getMaxSpeed();
+}
